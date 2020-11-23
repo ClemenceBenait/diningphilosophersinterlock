1107a0ec31004910f39e02a63173c69ed7f61179
ClemenceBenait <58024998+ClemenceBenait@users.noreply.github.com>
2020-11-23 16:05:05 +0100

    Initial commit

---
+++ src/main/java/diningphilosophers/ChopStick.java
@@ -0,0 +1,34 @@
+package diningphilosophers;
+
+public class ChopStick {
+    // Le nombre total de baguettes
+    private static int stickCount = 0;
+    // Le numéro de chaque baguette
+    private final int myNumber;
+    // Est-ce que ma baguette est libre ?
+    private boolean iAmFree = true;
+
+    public ChopStick() {
+        // Chaque baguette est numérotée 
+        myNumber = ++stickCount;
+    }
+
+    public void take() {
+        iAmFree = false;
+
+    }
+    
+    public void release() {
+        iAmFree = true;
+
+    }
+    
+    @Override
+    public String toString() {
+        return "Stick#" + myNumber;
+    }
+    
+    public boolean Statut() {
+        return iAmFree;
+    }
+}
